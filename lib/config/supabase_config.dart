272eb59710210ae8ea849b1b97d5387605a28e17
Fitahiana <rakotosalamafitahiana@gmail.com>
2024-11-07 13:18:44 +0300

    change supabase configuration

--- lib/config/supabase_config.dart
+++ lib/config/supabase_config.dart
@@ -1,8 +1,8 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseConfig {
-  static const String SUPABASE_URL = 'https://npmtlpurofsfuwqsbgwy.supabase.co';
-  static const String SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5wbXRscHVyb2ZzZnV3cXNiZ3d5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk4MjgxMjQsImV4cCI6MjA0NTQwNDEyNH0.zJXAC-xf_tRSNTNReFhYp-WG_Z7b4W93bPZQFP2L6Jg';
+  static const String SUPABASE_URL = 'https://rxednrvjlwazhcnewnnm.supabase.co';
+  static const String SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ4ZWRucnZqbHdhemhjbmV3bm5tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA5NDU1ODEsImV4cCI6MjA0NjUyMTU4MX0.XwDkBkK2LEkZQ98ex7e390zxn4k2K_7TVa4EvnvsHuc';
 
   static Future<void> initialize() async {
     await Supabase.initialize(
